b3f2bc251c1ab8237a03ec20688b90e844ef21ea
Tom de Grunt <tom@degrunt.nl>
2020-03-06 20:26:46 +0000

    Linting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "decoro"
+# frozen_string_literal: true
 
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'decoro'
+
+require 'minitest/autorun'
