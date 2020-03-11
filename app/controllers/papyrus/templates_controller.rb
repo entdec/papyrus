b31eb18410e7e01e68bf4bc4ac5212e74a50f34d
Tom de Grunt <tom@degrunt.nl>
2020-03-11 13:28:27 +0000

    Add warning, plus locale

--- app/controllers/papyrus/templates_controller.rb
+++ app/controllers/papyrus/templates_controller.rb
@@ -8,12 +8,21 @@ module Papyrus
 
     def paper
       template = Template.find(params[:id])
-      data = if request.post?
-               params[:context]
-             else
-               HashWithIndifferentAccess.new(template.example_data)[:context]
-             end
-      send_data template.render(data), type: 'application/pdf', disposition: 'inline', filename: template.file_name
+
+      ctx = params[:context]
+      locale = params[:locale]
+
+      if request.get?
+        example_data = HashWithIndifferentAccess.new(template.example_data)
+        ctx = example_data[:context]
+        locale = example_data[:locale]
+      end
+
+      data = I18n.with_locale(locale) do
+        template.render(ctx)
+      end
+
+      send_data data, type: 'application/pdf', disposition: 'inline', filename: template.file_name
     end
   end
 end
