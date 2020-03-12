b7787ca14dd9d9dc543608d6a17b1b511fd66935
Tom de Grunt <tom@degrunt.nl>
2020-03-12 11:35:28 +0000

    Fix

--- db/migrate/20200311190329_create_papyrus_papers.rb
+++ db/migrate/20200311190329_create_papyrus_papers.rb
@@ -4,7 +4,7 @@ class CreatePapyrusPapers < ActiveRecord::Migration[6.0]
   def change
     create_table :papyrus_papers, id: :uuid do |t|
       t.jsonb :data
-      t.references :template, null: false, foreign_key: true, type: :uuid, foreign_key: { to_table: :papyrus_templates }
+      t.references :template, null: false, type: :uuid, foreign_key: { to_table: :papyrus_templates }
 
       t.timestamps
     end
