fbfb263b84b3cdfa7669769d73db99050cb13d11
Tom de Grunt <tom@degrunt.nl>
2020-03-12 09:03:48 +0000

    Fix this too!

--- lib/papyrus/attachments_helpers.rb
+++ lib/papyrus/attachments_helpers.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-module Papyrus::AttachmentsHelpers
-  extend ActiveSupport::Concern
-  included do
-    has_many_attached :attachments
+module Papyrus
+  module AttachmentsHelpers
+    extend ActiveSupport::Concern
+    included do
+      has_many_attached :attachments
+    end
   end
 end
