decae8ba84e1e9b30a3b0b5884e83287dd780671
Tom de Grunt <tom@degrunt.nl>
2021-03-22 10:38:01 +0100

    Optional template

--- app/models/papyrus/paper.rb
+++ app/models/papyrus/paper.rb
@@ -2,7 +2,7 @@
 
 module Papyrus
   class Paper < ApplicationRecord
-    belongs_to :template
+    belongs_to :template, optional: true
     belongs_to :papyrable, polymorphic: true, optional: true
     belongs_to :owner, polymorphic: true, optional: true
 
