9e4407ee9bc64ebdbd69c9240932adda96eaff58
Tom de Grunt <tom@degrunt.nl>
2021-05-07 13:25:40 +0200

    Deprecator

--- lib/papyrus/deprecator.rb
+++ lib/papyrus/deprecator.rb
@@ -1,6 +1,8 @@
-class Papyrus::Deprecator
-  def deprecation_warning(deprecated_method_name, message, _caller_backtrace = nil)
-    message = "#{deprecated_method_name} is deprecated and will be removed from Papyrus | #{message}"
-    Kernel.warn message
+module Papyrus
+  class Deprecator
+    def deprecation_warning(deprecated_method_name, message, _caller_backtrace = nil)
+      message = "#{deprecated_method_name} is deprecated and will be removed from Papyrus | #{message}"
+      Kernel.warn message
+    end
   end
 end
