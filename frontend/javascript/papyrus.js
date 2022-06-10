f038ff46d0aa882b76646502fe26e16916853338
Tom de Grunt <tom@degrunt.nl>
2022-06-10 16:18:34 +0200

    Remove print channel, simplify start

--- frontend/javascript/papyrus.js
+++ frontend/javascript/papyrus.js
@@ -1,12 +1,8 @@
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import { Application } from "stimulus"
 
-// https://www.neodynamic.com/Products/Help/JSPrintManager4.0/apiref/modules/jspm.html
-import JSPM from "jsprintmanager"
-window.JSPM = JSPM
-
 export class Papyrus {
-  static start(application, startPrintManager = false, autoReconnect = false) {
+  static start(application) {
     if (!application) {
       application = Application.start()
     }
@@ -16,14 +12,5 @@ export class Papyrus {
     this.application = application
     const context = require.context("./controllers", true, /\.js$/)
     this.application.load(definitionsFromContext(context))
-
-    if (startPrintManager) {
-      // FIXME: This needs papyrus mounted in /papers
-      JSPM.JSPrintManager.license_url = `${location.protocol}//${location.host}/papers/print_client_license`
-      JSPM.JSPrintManager.auto_reconnect = autoReconnect
-      JSPM.JSPrintManager.start()
-      console.log("JSPM version:", JSPM.VERSION)
-      import("./channels")
-    }
   }
 }
