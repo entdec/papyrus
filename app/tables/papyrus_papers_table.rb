a7e0709ce17fb9a09ff7d31402e951b7df9207f1
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2022-11-08 15:38:27 +0400

    fix paper double regeneration

--- app/tables/papyrus_papers_table.rb
+++ app/tables/papyrus_papers_table.rb
@@ -21,12 +21,12 @@ class PapyrusPapersTable < ActionTable::ActionTable
                      title: t('papyrus.paper_table.preview'), target: '_blank')
       if row.owner
         concat link_to(content_tag(:i, nil, class: 'fal fa-print'), papyrus.print_paper_path(row.id),
-                       title: t('papyrus.paper_table.print'), data: { method: :post, turbo_method: :post })
+                       title: t('papyrus.paper_table.print'), data: { turbo_method: :post })
       end
 
       if row.template&.event.present?
         concat link_to(content_tag(:i, nil, class: 'fal fa-rotate-right'), papyrus.regenerate_paper_path(row),
-                       title: t('papyrus.paper_table.regenerate'), data: { method: :post, turbo_method: 'post' })
+                       title: t('papyrus.paper_table.regenerate'), data: { turbo_method: :post })
       end
     end
   end
