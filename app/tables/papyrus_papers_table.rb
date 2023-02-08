52752c2f69d635e8fe9c827b13928e0e86a33ea2
shubhrathaseetharamasetty <S.Seetharamasetty@rapidlogistics.nl>
2023-02-08 17:49:15 +0400

    attachment_sorting

--- app/tables/papyrus_papers_table.rb
+++ app/tables/papyrus_papers_table.rb
@@ -48,7 +48,7 @@ class PapyrusPapersTable < ActionTable::ActionTable
     elsif params[:papyrable]
       @scope = @scope.where(papyrable: params[:papyrable])
     end
-    @scope = @scope.joins(attachment_attachment: :blob) if params[:order_field_name] = 'attachment'
+    @scope = @scope.joins(attachment_attachment: :blob) if params[:order_field_name] == 'attachment'
     @scope = @scope.where(template_id: params[:template_id]) if params[:template_id].present?
     @scope
   end
