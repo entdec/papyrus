563611e98f685b5599b50de768c4f0b959eed827
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2023-08-04 17:55:49 +0400

    Added safe checkes

--- app/jobs/papyrus/generate_job.rb
+++ app/jobs/papyrus/generate_job.rb
@@ -1,7 +1,7 @@
 module Papyrus
   class GenerateJob < ApplicationJob
     def perform(event, obj = {}, params = {})
-      model = %w[obj_id obj_class].eql?(obj.keys) ? obj['obj_class'].constantize.find(obj['obj_id']) : obj
+      model = %w[obj_id obj_class].eql?(obj.keys) ? obj['obj_class'].safe_constantize&.find(obj['obj_id']) : obj
       return unless model
 
       generator = Papyrus::BaseGenerator.generator_for_obj(model).new(model, event, params)
--- app/jobs/papyrus/spool_print_job.rb
+++ app/jobs/papyrus/spool_print_job.rb
@@ -1,7 +1,8 @@
 module Papyrus
   class SpoolPrintJob < ApplicationJob
     def perform(print_job_id)
-      print_job = Papyrus::PrintJob.find(print_job_id)
+      print_job = Papyrus::PrintJob.find_by(id: print_job_id)
+      return if print_job.blank?
       print_job.spool!
     end
   end
