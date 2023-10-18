738334d366596e354a41329b5c7538af935e4ae4
Joyie Echteld <69836334+plummip@users.noreply.github.com>
2023-10-18 21:44:24 +0200

    remove catch of exceptions. if we do this the job wont be put in the retry queue

--- lib/papyrus/consolidation/sidekiq_server_middleware.rb
+++ lib/papyrus/consolidation/sidekiq_server_middleware.rb
@@ -12,30 +12,25 @@ module Papyrus
       def call(job_instance, job_payload, queue)
         papyrus_datastore = (job_payload['$papyrus_datastore'] if job_payload['class'] != 'Sidekiq::Batch::Empty')
 
-        begin
-          if Papyrus.papyrus_datastore.empty? && papyrus_datastore.present?
-            Thread.current[:papyrus_master_job] ||= job_instance.object_id
-            Papyrus.add_thread_variables(**papyrus_datastore)
+        if Papyrus.papyrus_datastore.empty? && papyrus_datastore.present?
+          Thread.current[:papyrus_master_job] ||= job_instance.object_id
+          Papyrus.add_thread_variables(**papyrus_datastore)
 
-            # If we are NOT within a Sidekiq batch, we need to prepend the batch class methods.w
-            # We don't want to put yield in a batch directly as it might not be the last middleware in the chain.
-            if Thread.current[:sidekiq_batch].blank?
-              bid = (Thread.current[:sidekiq_context][:bid] if Thread.current[:sidekiq_context].present?)
-              job_instance.class.prepend(Papyrus::Consolidation::BatchClassMethods) if bid.present?
-            end
-          end
-
-          # Go on to the next middleware in the chain or the worker `perform` method
-          yield
-        rescue StandardError => e
-          Papyrus.logger.error e.message
-        ensure
-          if Thread.current[:papyrus_master_job] == job_instance.object_id
-            Papyrus.remove_datastore
-            Thread.current[:papyrus_master_job] = nil
+          # If we are NOT within a Sidekiq batch, we need to prepend the batch class methods.w
+          # We don't want to put yield in a batch directly as it might not be the last middleware in the chain.
+          if Thread.current[:sidekiq_batch].blank?
+            bid = (Thread.current[:sidekiq_context][:bid] if Thread.current[:sidekiq_context].present?)
+            job_instance.class.prepend(Papyrus::Consolidation::BatchClassMethods) if bid.present?
           end
         end
 
+        # Go on to the next middleware in the chain or the worker `perform` method
+        yield
+      ensure
+        if Thread.current[:papyrus_master_job] == job_instance.object_id
+          Papyrus.remove_datastore
+          Thread.current[:papyrus_master_job] = nil
+        end
       end
 
     end
