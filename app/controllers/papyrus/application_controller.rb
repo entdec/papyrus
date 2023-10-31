30a9880e7bebe8f1a7bef83b47d28088634b052a
shubh <S.Seetharamasetty@rapidlogistics.nl>
2023-10-31 16:47:56 +0400

    active_record_not_found

--- app/controllers/papyrus/application_controller.rb
+++ app/controllers/papyrus/application_controller.rb
@@ -6,5 +6,12 @@ module Papyrus
     respond_to :html
 
     protect_from_forgery with: :exception
+    rescue_from ActiveRecord::RecordNotFound, with: :redirect_to_root
+
+    private
+    def redirect_to_root(_exception)
+      Signum.error(current_user, text: t("errors.messages.no_access_redirecting"))
+      redirect_to root_url, status: :see_other
+    end
   end
 end
