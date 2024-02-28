ef00f8cc68f1084ba0701de24635fa611ec0b04b
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2024-02-28 14:57:36 +0400

    Also modified preferred_printer key

--- app/models/papyrus/computer.rb
+++ app/models/papyrus/computer.rb
@@ -1,6 +1,6 @@
 module Papyrus
   class Computer < ApplicationRecord
     has_many :printers, class_name: 'Printer', foreign_key: 'computer_id', dependent: :destroy
-    has_many :preferred_printers, class_name: 'PreferredPrinter', foreign_key: 'printer_id', dependent: :destroy
+    has_many :preferred_printers, class_name: 'PreferredPrinter', foreign_key: 'computer_id', dependent: :destroy
   end
 end
