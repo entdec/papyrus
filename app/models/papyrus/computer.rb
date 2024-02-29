7ac1fd0a4c70b69649a07eb524675bd0d1c4a963
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2024-02-29 17:32:28 +0400

    Fixed dependent user nullify on computer delete

--- app/models/papyrus/computer.rb
+++ app/models/papyrus/computer.rb
@@ -2,5 +2,6 @@ module Papyrus
   class Computer < ApplicationRecord
     has_many :printers, class_name: 'Printer', foreign_key: 'computer_id', dependent: :destroy
     has_many :preferred_printers, class_name: 'PreferredPrinter', foreign_key: 'computer_id', dependent: :destroy
+    has_many :users, class_name: 'User', foreign_key: "current_computer_id", dependent: :nullify
   end
 end
