5a2125c105eabea2146a82316327ab8b6d24999d
Tom de Grunt <tom@degrunt.nl>
2024-03-03 14:00:46 +0100

    Fix initializer

--- lib/generators/papyrus/templates/config/initializers/papyrus.rb
+++ lib/generators/papyrus/templates/config/initializers/papyrus.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-Vorto.setup do |config|
-  config.base_controller = "::ApplicationController"
-  config.admin_authentication_module = "Auxilium::Concerns::AdminAuthenticated"
+Papyrus.setup do |config|
+  config.base_controller = '::ApplicationController'
+  config.admin_authentication_module = 'Auxilium::Concerns::AdminAuthenticated'
 
   config.logger = Rails.logger
 end
