8349089f4153dec2aed4d9a2bcde89162a986597
Joyie Echteld <69836334+plummip@users.noreply.github.com>
2024-11-06 10:26:46 +0100

    ensure job logger has been added properly and is in the right folder

--- lib/papyrus/job_perform_logger.rb
+++ lib/papyrus/job_perform_logger.rb
@@ -1,25 +1,28 @@
-module JobPerformLogger
-  def self.included(base)
-    base.extend ClassMethods
-  end
+module Papyrus
+  module JobPerformLogger
 
-  module ClassMethods
-    def included(mod)
-      super
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
-      mod.class_eval do
-        if defined?(Sidekiq::Job) && mod.included_modules.include?(Sidekiq::Job) && !mod.included_modules.include?(Papyrus::JobPerformLogger)
-          alias_method :perform_inline_without_logging, :perform_inline
-          alias_method :perform_inline, :perform_inline_with_logging
+    module ClassMethods
+      def included(mod)
+        super
+
+        mod.class_eval do
+          if defined?(Sidekiq::Job) && mod.included_modules.include?(Sidekiq::Job) && !mod.included_modules.include?(Papyrus::JobPerformLogger)
+            alias_method :perform_inline_without_logging, :perform_inline
+            alias_method :perform_inline, :perform_inline_with_logging
+          end
         end
       end
     end
-  end
 
-  def perform_inline_with_logging(*args)
-    Thread.current[:sidekiq_job_sync_execution] = true
-    perform_inline_without_logging(*args)
-  ensure
-    Thread.current[:sidekiq_job_sync_execution] = false
+    def perform_inline_with_logging(*args)
+      Thread.current[:sidekiq_job_sync_execution] = true
+      perform_inline_without_logging(*args)
+    ensure
+      Thread.current[:sidekiq_job_sync_execution] = false
+    end
   end
 end
